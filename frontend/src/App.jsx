4b9e0f254dca235f6861c49a932dfcf2ece9b74a
varshh7 <varshasoundar4@gmail.com>
2024-10-17 20:12:19 +0530

    commited

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,6 +17,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
+
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
