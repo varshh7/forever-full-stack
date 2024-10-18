fbc18edd2d0635d55fa44d326b6299d16f0a584c
varshh7 <varshasoundar4@gmail.com>
2024-10-18 09:36:06 +0530

    commited

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: { port: 5173 },
   build: {
     rollupOptions: {
-      external: ['react-toastify'], // Add this line
-    },
-  },
+      // Externalize axios to avoid bundling it
+      external: ['axios','react-toastify']
+    }
+  }
 });
