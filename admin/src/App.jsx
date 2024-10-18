d27198afb90c6ece91f8f7c7e714c150cd56a676
varshh7 <varshasoundar4@gmail.com>
2024-10-18 09:40:00 +0530

    commited

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,12 +6,12 @@ import Add from './pages/Add'
 import List from './pages/List'
 import Orders from './pages/Orders'
 import Login from './components/Login'
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 
 export const backendUrl=import.meta.env.VITE_BACKEND_URL
-export const currency=" $"
+export const currency="$"
 const App = () => {
 
   const [token,setToken]=useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
