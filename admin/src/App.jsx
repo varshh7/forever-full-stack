f1873a2a9c381cd4fe059a36e698c3897e1fb3f2
varshh7 <varshasoundar4@gmail.com>
2024-10-19 15:00:35 +0530

    commited

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,7 +7,7 @@ import List from './pages/List'
 import Orders from './pages/Orders'
 import Login from './components/Login'
 import { ToastContainer} from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+//import 'react-toastify/dist/ReactToastify.css';
 
 
 export const backendUrl=import.meta.env.VITE_BACKEND_URL
